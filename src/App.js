6add20e44e240511ed17064c9bc3cf4327b60050
Bibekanand Kushwaha <anandkushwaha2074@gmail.com>
2021-10-07 22:08:01 +0545

    Learning about JSX

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="blank">
+      <h1>This is my new site</h1>
+    </div>
+  );
+}
+
+export default App;

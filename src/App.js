c233dd0244dd25c7db8b3702a844a78225f41bd1
Bibekanand Kushwaha <anandkushwaha2074@gmail.com>
2021-10-11 08:40:54 +0545

    text utils app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import React, {useState} from 'react';
+
+let title = {"name":"Text Utils", "url":"/"};
+let home = {"name":"Home", "url":"/home"};
+let about = {"name":"About", "url":"/about"};
+let contact = {"name":"Contact", "url":"/contact"};
+
 
 function App() {
+  const [darkMode, setDarkMode] = useState('light');
+
+  const toggleDarkMode = ()=>{
+    if(darkMode==='light'){
+      setDarkMode('dark');
+      document.body.style.backgroundColor='#212529'
+    }
+    else{
+      setDarkMode('light');
+      document.body.style.backgroundColor='white'
+    }
+  }
   return (
-    <div className="blank">
-      <h1>This is my new site</h1>
-    </div>
+    <>
+      <Navbar toggleDarkMode={toggleDarkMode} darkMode = {darkMode} title={title} home={home} about={about} contact={contact} />
+      <TextForm darkMode={darkMode} heading="Enter your text" />
+    </>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,86 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export default function Navbar(props) {
+  return (
+    <nav
+      className={`navbar navbar-expand-lg navbar-${props.darkMode} bg-${
+        props.darkMode === "light" ? "light" : "dark"
+      } shadow`}
+    >
+      <div className="container-fluid">
+        <a className="navbar-brand" href={props.title.url}>
+          {props.title.name}
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="/">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link"
+                aria-current="page"
+                href={props.about.url}
+              >
+                {props.about.name}
+              </a>
+            </li>
+          </ul>
+          <form className="d-flex">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
+          {/* Dark mode button */}
+            <label className={`form-check-label mx-2 text-${props.darkMode==='light'?'dark':'light'}`}>
+              Light
+            </label>
+          <div className="form-check form-switch">
+            <input
+            onClick={props.toggleDarkMode}
+              className="form-check-input"
+              type="checkbox"
+              role="switch"
+            />
+            <label className={`form-check-label mx-2 text-${props.darkMode==='light'?'dark':'light'}`}>
+              Dark
+            </label>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+// Proptypes is just to validate the props that are passed to the component
+Navbar.propTypes = {
+  title: PropTypes.object.isRequired,
+  about: PropTypes.object.isRequired,
+};
+
+// set default props
+Navbar.defaultProps = {
+  title: { url: "/", name: "Title" },
+  about: { url: "/about", name: "About" },
+  home: { url: "/", name: "Home" },
+};
